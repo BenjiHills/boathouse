ca5498933173f3f3d463774ac7feb110f1c29958
BenjiHills <benhills@outlook.com>
2022-03-06 13:18:35 +0000

    add login page, navigation bar and permissions security to selected views

--- takeaway/menu/views.py
+++ takeaway/menu/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .forms import *
 from .models import *
@@ -8,6 +10,10 @@ from django.db.models import Case, When, Value
 def form_valid(self, form):
   form.instance.user = self.request.user
   return form_valid(form)
+
+def LogoutView(request):
+  logout(request)
+  return HttpResponseRedirect("/")
 
 class AccountCreateView(CreateView):
   
@@ -31,9 +37,9 @@ class HomeView(ListView):
         )
         )
 
-class CreateOrderView(CreateView):
+class CreateOrderView(LoginRequiredMixin, CreateView):
     model = Order
-    template_name = "menu/create.html"
+    template_name = "menu/create_order.html"
     form_class = OrderCreateForm
     
     def get_success_url(self):
@@ -43,12 +49,10 @@ class CreateOrderView(CreateView):
         form.instance.customer = self.request.user
         return super().form_valid(form)
 
-
-
-class CheckoutView(UpdateView):
+class CheckoutView(LoginRequiredMixin, UpdateView):
     model = Order
     template_name = "menu/checkout.html"
-    form_class = CheckoutForm 
+    form_class = CheckoutForm
     success_url = "/"
 
     def form_valid(self, form):
@@ -56,7 +60,9 @@ class CheckoutView(UpdateView):
             form.instance.status = "PE"
         return super().form_valid(form)
 
-class DishView(CreateView):
+class DishView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    permission_required = "menu.add_dish"
+    raise_exception = True
     model = Dish
     template_name = "menu/dishes.html"
     form_class = CreateDishForm 
@@ -71,7 +77,9 @@ class DishView(CreateView):
         form.instance.descrip = form.instance.descrip.capitalize()
         return super().form_valid(form)
 
-class CustomerOrderView(ListView):
+class CustomerOrderView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    permission_required = "menu.change_order"
+    raise_exception = True
     model = Order
     template_name = "menu/orders.html"
 
@@ -79,7 +87,9 @@ class CustomerOrderView(ListView):
         return Order.objects.filter(status__in = ["PE", "DL", "CL"]).order_by("time")
 
   
-class StatusUpdateView(UpdateView):
+class StatusUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    permission_required = "menu.change_order"
+    raise_exception = True
     model = Order
     template_name = "menu/order_status.html"
     form_class = OrderStatusForm
@@ -95,25 +105,29 @@ class StatusUpdateView(UpdateView):
          
         return super().form_valid(form)
 
-class MyOrderView(ListView):
+class MyOrderView(LoginRequiredMixin, ListView):
         model = Order
         template_name = "menu/my_orders.html"
 
         def get_queryset(self):
             return Order.objects.filter(customer = self.request.user).order_by("-time")
 
-class DishDeleteView(DeleteView): 
+class DishDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView): 
+        permission_required = "menu.delete_dish"
+        raise_exception = True
         model = Dish
         template_name = "menu/dish_delete.html"
         success_url = "/dishes"
 
-class DishUpdateView(UpdateView):
+class DishUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    permission_required = "menu.delete_dish"
+    raise_exception = True
     model = Dish
     template_name = "menu/dish_update.html"
     form_class = DishUpdateForm 
     success_url = "/dishes"
 
-class OrderDeleteView(DeleteView): 
+class OrderDeleteView(LoginRequiredMixin,DeleteView): 
         model = Order
         template_name = "menu/order_delete.html"
         success_url = "/"
